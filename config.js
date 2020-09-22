f7d6c6ee36382b47544e42b06f12b1e03b3c3f90
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-22 22:13:26 +0200

    Improve short name

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: "Bestattungen Hochuli",
   manifestName: "Prologue",
-  manifestShortName: "Landing", // max 12 characters
+  manifestShortName: "Landing Page", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: "#663399",
   manifestThemeColor: "#663399",
