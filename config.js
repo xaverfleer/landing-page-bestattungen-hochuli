7f71bd82b4a6122dac9361bc30252e36336a6444
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-22 19:39:23 +0200

    Format with Prettier

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Prologue', // <title>
-  manifestName: 'Prologue',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  siteTitle: "Gatsby Starter Prologue", // <title>
+  manifestName: "Prologue",
+  manifestShortName: "Landing", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: "#663399",
+  manifestThemeColor: "#663399",
+  manifestDisplay: "standalone",
+  manifestIcon: "src/assets/img/website-icon.png",
   pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Xaver Fleer',
-  clientName: 'Bestattungen Hochuli',
-  heading: 'Kanton Bern und Umgebung',
+  authorName: "Xaver Fleer",
+  clientName: "Bestattungen Hochuli",
+  heading: "Kanton Bern und Umgebung",
   // social
   socialLinks: [
     {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/xaverfleer',
+      icon: "fa-twitter",
+      name: "Twitter",
+      url: "https://twitter.com/xaverfleer",
     },
     {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/xaverfleer',
+      icon: "fa-facebook",
+      name: "Facebook",
+      url: "https://facebook.com/xaverfleer",
     },
     {
-      icon: 'fa-envelope-o',
-      name: 'Email',
-      url: 'mailto:xaver.fleer@gmail.com',
+      icon: "fa-envelope-o",
+      name: "Email",
+      url: "mailto:xaver.fleer@gmail.com",
     },
   ],
 };
