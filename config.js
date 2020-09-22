6233ecc3a775908111fac26cc62d1a9a431fe8cd
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-22 23:00:39 +0200

    Rename favicon

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: "#663399",
   manifestThemeColor: "#663399",
   manifestDisplay: "standalone",
-  manifestIcon: "src/assets/img/website-icon.gif",
+  manifestIcon: "src/assets/images/favicon.gif",
   pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: "Xaver Fleer",
   clientName: "Hochuli Bestattungen",
