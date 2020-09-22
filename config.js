d0803fabe6b688d981249feb803008ccf782a262
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-22 22:56:20 +0200

    Update favicon

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: "#663399",
   manifestThemeColor: "#663399",
   manifestDisplay: "standalone",
-  manifestIcon: "src/assets/img/website-icon.png",
+  manifestIcon: "src/assets/img/website-icon.gif",
   pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: "Xaver Fleer",
   clientName: "Bestattungen Hochuli",
