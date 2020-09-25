d6a802483e9ce78bbc42acbf14c69a83cdbf8910
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-25 14:30:16 +0200

    Formatting with prettier

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
 
-import '../assets/sass/main.scss';
+import "../assets/sass/main.scss";
 
 class Layout extends Component {
   constructor(props) {
@@ -39,18 +39,18 @@ class Layout extends Component {
             }
           }
         `}
-        render={data => (
+        render={(data) => (
           <>
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Eventually' },
-                { name: 'keywords', content: 'site, web' },
+                { name: "description", content: "Eventually" },
+                { name: "keywords", content: "site, web" },
               ]}
             >
               <html lang="en" />
             </Helmet>
-            <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
+            <div className={isPreloaded ? "main-body is-preload" : "main-body"}>
               {children}
             </div>
           </>
