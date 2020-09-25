351687218327a5801aaabbf3f2eef46c661cfc00
Xaver Fleer <xaver.fleer@gmail.com>
2020-09-25 14:17:24 +0200

    Format with Prettier

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Footer from './Footer';
-import Header from './Header';
-import Nav from './Nav';
-import TopNav from './TopNav';
-import config from '../../../config';
-const pic = require('../../assets/images/BeniHochuli.gif');
+import React, { useState } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Nav from "./Nav";
+import TopNav from "./TopNav";
+import config from "../../../config";
+const pic = require("../../assets/images/BeniHochuli.gif");
 
 export default function SideBar({ sections = [] }) {
   const [headerOpen, toggleHeader] = useState(false);
   return (
-    <div className={`${headerOpen ? 'header-visible' : ' '}`}>
+    <div className={`${headerOpen ? "header-visible" : " "}`}>
       <TopNav
         title={config.authorName}
         onMenuClick={() => toggleHeader(!headerOpen)}
